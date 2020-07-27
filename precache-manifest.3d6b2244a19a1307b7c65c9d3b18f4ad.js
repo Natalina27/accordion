a25b96bdb81b8bc771c5c422ee0d3ae9ef88176f
Natalya Myunster <natalyamyunster@yahoo.com>
2020-07-27 11:22:55 +0200

    Updates

---
+++ precache-manifest.3d6b2244a19a1307b7c65c9d3b18f4ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9a6b6ba2460d20b9064135b895379a",
+    "url": "/accordion/index.html"
+  },
+  {
+    "revision": "4fa3c34f00e7d06f7ae3",
+    "url": "/accordion/static/css/main.98a47c93.chunk.css"
+  },
+  {
+    "revision": "c483741f31ddf6b5c7f7",
+    "url": "/accordion/static/js/2.25b87085.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/accordion/static/js/2.25b87085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa3c34f00e7d06f7ae3",
+    "url": "/accordion/static/js/main.03067766.chunk.js"
+  },
+  {
+    "revision": "14eea8dc2c456fa5ba9c",
+    "url": "/accordion/static/js/runtime-main.d40e25f1.js"
+  }
+]);
